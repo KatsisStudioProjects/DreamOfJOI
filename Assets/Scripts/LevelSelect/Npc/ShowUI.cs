97be85a6b428d51faeba173c2287db2b0a851dd6
Xwilarg <xwilarg@protonmail.com>
2024-01-13 14:46:34 +0100

    Link modifier panel to level selector

---
+++ Assets/Scripts/LevelSelect/Npc/ShowUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace NsfwMiniJam.LevelSelect.Npc
+{
+    public class ShowUI : AInteractible
+    {
+        [SerializeField]
+        private GameObject _go;
+
+        public override void Interact()
+        {
+            _go.SetActive(true);
+        }
+
+        public override void InteractionCancel()
+        {
+            _go.SetActive(false);
+        }
+    }
+}
