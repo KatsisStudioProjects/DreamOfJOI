2ec625ae1626d8ffc1bcb61bf49d81fffd851eb1
Xwilarg <xwilarg@protonmail.com>
2024-01-13 13:11:39 +0100

    Add basic player controller

---
+++ Assets/Scripts/LevelSelect/LevelSelectManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NsfwMiniJam.LevelSelect
+{
+    public class LevelSelectManager : MonoBehaviour
+    {
+        private void Awake()
+        {
+            SceneManager.LoadScene("AchievementManager", LoadSceneMode.Additive);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelSelect/Player/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NsfwMiniJam.LevelSelect.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        private Vector2 _mov;
+
+        private Rigidbody2D _rb;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = _mov * 10f;
+        }
+
+        public void OnMove(InputAction.CallbackContext value)
+        {
+            _mov = value.ReadValue<Vector2>();
+        }
+    }
+}
