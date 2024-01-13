a381c5dda52cff59c071974f06c6f7c53cc5856c
Xwilarg <xwilarg@protonmail.com>
2024-01-13 02:20:05 +0100

    Add basic note spawning

--- Assets/Scripts/Rhythm/RhythmManager.cs
+++ Assets/Scripts/Rhythm/RhythmManager.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,6 +12,45 @@ namespace NsfwMiniJam.Rhythm
 
         [SerializeField]
         private GameObject _note;
+
+        private const float _offsetBeforeStart = 3f;
+
+        private int _spawnIndex;
+        private float _startTime;
+
+        private float _bpm = 60f;
+
+        private const float _height = 2000f; // TODO: ewh
+
+        private readonly List<GameObject> _notes = new();
+
+        private void Awake()
+        {
+            _startTime = Time.unscaledTime;
+            SpawnNotes();
+        }
+
+        private void Update()
+        {
+            foreach (var n in _notes)
+            {
+                n.transform.Translate(Vector2.down * _bpm * Time.deltaTime);
+            }
+        }
+
+        private void SpawnNotes()
+        {
+            var elapsedTime = Time.unscaledTime - _startTime;
+
+            for (float i = 0f; i < _height; i += _bpm)
+            {
+                var n = Instantiate(_note, _noteContainer);
+
+                ((RectTransform)n.transform).anchoredPosition = Vector2.up * i;
+
+                _notes.Add(n);
+            }
+        }
 
         public void OnHit(InputAction.CallbackContext value)
         {
