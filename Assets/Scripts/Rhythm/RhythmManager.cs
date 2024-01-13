3cd2ab5308ba3a6abdb285b489fd45cf048464cb
Xwilarg <xwilarg@protonmail.com>
2024-01-13 02:38:40 +0100

    Make notes spawn and despawn properly

--- Assets/Scripts/Rhythm/RhythmManager.cs
+++ Assets/Scripts/Rhythm/RhythmManager.cs
@@ -1,5 +1,6 @@
-using System.Collections;
+using NsfwMiniJam.SO;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +8,9 @@ namespace NsfwMiniJam.Rhythm
 {
     public class RhythmManager : MonoBehaviour
     {
+        [SerializeField]
+        private GameInfo _info;
+
         [SerializeField]
         private RectTransform _noteContainer;
 
@@ -18,11 +22,11 @@ namespace NsfwMiniJam.Rhythm
         private int _spawnIndex;
         private float _startTime;
 
-        private float _bpm = 60f;
+        private float _bpm = 300f;
 
         private const float _height = 2000f; // TODO: ewh
 
-        private readonly List<GameObject> _notes = new();
+        private readonly List<RectTransform> _notes = new();
 
         private void Awake()
         {
@@ -36,19 +40,31 @@ namespace NsfwMiniJam.Rhythm
             {
                 n.transform.Translate(Vector2.down * _bpm * Time.deltaTime);
             }
+
+            if (_notes[0].anchoredPosition.y < -_info.BadDistance)
+            {
+                Destroy(_notes[0].gameObject);
+                _notes.RemoveAt(0);
+                // TODO: Display "miss"
+            }
+
+            SpawnNotes();
         }
 
         private void SpawnNotes()
         {
-            var elapsedTime = Time.unscaledTime - _startTime;
+            var lastYPos = _notes.Any() ? _notes.Last().anchoredPosition.y : 0f;
 
-            for (float i = 0f; i < _height; i += _bpm)
+            for (float i = lastYPos + _bpm; i < _height; i += _bpm)
             {
                 var n = Instantiate(_note, _noteContainer);
 
-                ((RectTransform)n.transform).anchoredPosition = Vector2.up * i;
+                var rTransform = (RectTransform)n.transform;
+                rTransform.anchorMin = new(.5f, 0f);
+                rTransform.anchorMax = new(.5f, 0f);
+                rTransform.anchoredPosition = Vector2.up * i;
 
-                _notes.Add(n);
+                _notes.Add(rTransform);
             }
         }
 
---
+++ Assets/Scripts/SO/GameInfo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace NsfwMiniJam.SO
+{
+    [CreateAssetMenu(menuName = "ScriptableObject/GameInfo", fileName = "GameInfo")]
+    public class GameInfo : ScriptableObject
+    {
+        public float GreatDistance, GoodDistance, BadDistance;
+    }
+}
