017e35cca065c9bd9b0e3c29b83b9633fd5ff021
Xwilarg <xwilarg@protonmail.com>
2024-01-13 14:33:35 +0100

    Add all modifiers to menu

--- Assets/Scripts/LevelSelect/ModifierSelector.cs
+++ Assets/Scripts/LevelSelect/ModifierSelector.cs
@@ -8,7 +8,7 @@ namespace NsfwMiniJam.LevelSelect
     public class ModifierSelector : MonoBehaviour
     {
         [SerializeField]
-        private TMP_Text _txtSuddenDeath;
+        private TMP_Text _txtSuddenDeath, _txtHidden, _txtReversed, _txtMines;
 
         public void UpdateSuddenDeath()
         {
@@ -21,6 +21,29 @@ namespace NsfwMiniJam.LevelSelect
             UpdateUI();
         }
 
+        public void UpdateHidden()
+        {
+            GlobalData.Hidden++;
+            if (GlobalData.Hidden > Enum.GetValues(typeof(HiddenType)).Cast<HiddenType>().Max())
+            {
+                GlobalData.Hidden = 0;
+            }
+
+            UpdateUI();
+        }
+
+        public void UpdateReversed()
+        {
+            GlobalData.Reversed = !GlobalData.Reversed;
+            UpdateUI();
+        }
+
+        public void UpdateMines()
+        {
+            GlobalData.Mines = !GlobalData.Mines;
+            UpdateUI();
+        }
+
         private void UpdateUI()
         {
             _txtSuddenDeath.text = GlobalData.SuddenDeath switch
@@ -30,6 +53,15 @@ namespace NsfwMiniJam.LevelSelect
                 SuddenDeathType.PerfectOnly => "Perfect Only",
                 _ => throw new NotImplementedException()
             };
+            _txtHidden.text = GlobalData.Hidden switch
+            {
+                HiddenType.None => "Disabled",
+                HiddenType.Normal => "Enabled",
+                HiddenType.Reversed => "Reversed",
+                _ => throw new NotImplementedException()
+            };
+            _txtReversed.text = GlobalData.Reversed ? "Enabled" : "Disabled";
+            _txtMines.text = GlobalData.Mines ? "Enabled" : "Disabled";
         }
 
         private void OnEnable()
