e9a42569e39058639862cd66042c024609439a99
Xwilarg <xwilarg@protonmail.com>
2024-01-13 18:29:22 +0100

    Add simple VN system

---
+++ Assets/Scripts/VN/TextDisplay.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Text;
+using TMPro;
+using UnityEngine;
+
+namespace NsfwMiniJam.VN
+{
+    public class TextDisplay : MonoBehaviour
+    {
+        private TMP_Text _text;
+
+        private float _timer;
+
+        private int _index;
+
+        private const float _displaySpeedRef = .02f;
+
+        private string _toDisplay;
+        public string ToDisplay
+        {
+            set
+            {
+                _index = 0;
+                _timer = _displaySpeedRef;
+                _text.text = string.Empty;
+                _toDisplay = value.Replace("\r", ""); // Remove \r cause we don't care
+                SplitVertical();
+            }
+            private get => _toDisplay;
+        }
+
+        public event EventHandler OnDisplayDone;
+
+        private string _displayLeft = string.Empty;
+
+        public bool IsDisplayDone => _index == _toDisplay.Length && _displayLeft == string.Empty;
+
+        /// <summary>
+        /// Makes sure the current text vertically fit in the box
+        /// </summary>
+        private void SplitVertical()
+        {
+            StringBuilder tmp = new();
+            foreach (var word in _toDisplay.Split(' '))
+            {
+                var totalWidth = _text.GetPreferredValues($"{tmp} {word}");
+                if (totalWidth.x > _text.rectTransform.rect.width)
+                {
+                    totalWidth = _text.GetPreferredValues($"{tmp}\n{word}");
+                    if (totalWidth.y < _text.rectTransform.rect.height)
+                    {
+                        tmp.Append($"\n{word}");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                else if (totalWidth.y < _text.rectTransform.rect.height)
+                {
+                    tmp.Append($" {word}");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var res = tmp.ToString()[1..];
+            _displayLeft = _toDisplay[res.Length..];
+            _toDisplay = res.TrimStart();
+        }
+
+        /// <summary>
+        /// Either display the rest of the text or start with the remaining one
+        /// </summary>
+        public void ForceDisplay()
+        {
+            if (_index < _toDisplay.Length)
+            {
+                _text.text = _toDisplay;
+                _index = _toDisplay.Length;
+                if (IsDisplayDone)
+                {
+                    OnDisplayDone?.Invoke(this, new());
+                }
+            }
+            else
+            {
+                ToDisplay = _displayLeft;
+            }
+        }
+
+        private void Awake()
+        {
+            _text = GetComponent<TMP_Text>();
+        }
+
+        private void Update()
+        {
+            if (_toDisplay != null && _index < _toDisplay.Length)
+            {
+                _timer -= Time.deltaTime;
+                if (_timer <= 0f)
+                {
+                    _timer = _displaySpeedRef;
+                    _text.text += _toDisplay[_index];
+                    _index++;
+                    if (IsDisplayDone)
+                    {
+                        OnDisplayDone?.Invoke(this, new());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/VN/VNManager.cs
@@ -0,0 +1,104 @@
+﻿using Ink.Runtime;
+using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace NsfwMiniJam.VN
+{
+    public class VNManager : MonoBehaviour
+    {
+        public static VNManager Instance { private set; get; }
+
+        [Header("Components")]
+        [SerializeField]
+        private TextDisplay _display;
+
+        private Story _story;
+
+        [SerializeField]
+        private GameObject _container;
+
+        private bool _isSkipEnabled;
+        private float _skipTimer;
+        private float _skipTimerRef = .1f;
+
+        private Action _onDone;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        public bool IsPlayingStory => _container.activeInHierarchy;
+
+        private void Update()
+        {
+            if (_isSkipEnabled)
+            {
+                _skipTimer -= Time.deltaTime;
+                if (_skipTimer < 0)
+                {
+                    _skipTimer = _skipTimerRef;
+                    DisplayNextDialogue();
+                }
+            }
+        }
+
+        public void ShowStory(TextAsset asset, Action onDone)
+        {
+            Debug.Log($"[NOV] Playing {asset.name}");
+            _onDone = onDone;
+            _story = new(asset.text);
+            _isSkipEnabled = false;
+            DisplayStory(_story.Continue());
+        }
+
+        private void DisplayStory(string text)
+        {
+            _container.SetActive(true);
+
+            _display.ToDisplay = text;
+        }
+
+        public void DisplayNextDialogue()
+        {
+            if (!_container.activeInHierarchy)
+            {
+                return;
+            }
+            if (!_display.IsDisplayDone)
+            {
+                // We are slowly displaying a text, force the whole display
+                _display.ForceDisplay();
+            }
+            else if (_story.canContinue && // There is text left to write
+                !_story.currentChoices.Any()) // We are not currently in a choice
+            {
+                DisplayStory(_story.Continue());
+            }
+            else if (!_story.canContinue && !_story.currentChoices.Any())
+            {
+                _container.SetActive(false);
+                _onDone?.Invoke();
+            }
+        }
+
+        public void ToggleSkip(bool value)
+            => _isSkipEnabled = value;
+
+        public void OnToggleSkip(InputAction.CallbackContext value)
+        {
+            if (value.phase == InputActionPhase.Started) ToggleSkip(true);
+            else if (value.phase == InputActionPhase.Canceled) ToggleSkip(false);
+        }
+
+        public void OnNextDialogue(InputAction.CallbackContext value)
+        {
+            if (value.performed)
+            {
+                DisplayNextDialogue();
+            }
+        }
+    }
+}
