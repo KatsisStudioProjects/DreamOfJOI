327454c5730d42df2723565dc7961f96adc6c69a
Xwilarg <xwilarg@protonmail.com>
2024-01-18 13:43:12 +0100

    Make level unlock progressive

---
+++ Assets/Scripts/LevelSelect/HideIfNotUnlocked.cs
@@ -0,0 +1,27 @@
+﻿using NsfwMiniJam.Persistency;
+using UnityEngine;
+
+namespace NsfwMiniJam.LevelSelect
+{
+    public class HideIfNotUnlocked : MonoBehaviour
+    {
+        [SerializeField]
+        private int _index;
+
+        [SerializeField]
+        private bool _reverse;
+
+        private void Awake()
+        {
+            var savedData = PersistencyManager.Instance.SaveData.GetScore(_index);
+            if (savedData == null && !_reverse)
+            {
+                gameObject.SetActive(false);
+            }
+            else if (savedData != null && _reverse)
+            {
+
+            }
+        }
+    }
+}
