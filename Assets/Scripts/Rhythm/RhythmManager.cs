3b3421ff836d1a23e4f6f37bd1f2ab52d7cd82c7
Xwilarg <xwilarg@protonmail.com>
2024-01-18 17:23:29 +0100

    Fix custom data being saved

--- Assets/Scripts/Rhythm/RhythmManager.cs
+++ Assets/Scripts/Rhythm/RhythmManager.cs
@@ -509,8 +509,11 @@ namespace NsfwMiniJam.Rhythm
             _victory.gameObject.SetActive(true);
             _victory.Init(Music, _score, _maxPossibleScore, _info, _combo == Music.NoteCount);
 
-            PersistencyManager.Instance.SaveData.AddScore(GlobalData.LevelIndex, new() { Score = _score / (float)_maxPossibleScore, Multiplier = GlobalData.CalculateMultiplier(), IsFullCombo = _combo == Music.NoteCount });
-            PersistencyManager.Instance.Save();
+            if (GlobalData.LevelIndex != -1)
+            {
+                PersistencyManager.Instance.SaveData.AddScore(GlobalData.LevelIndex, new() { Score = _score / (float)_maxPossibleScore, Multiplier = GlobalData.CalculateMultiplier(), IsFullCombo = _combo == Music.NoteCount });
+                PersistencyManager.Instance.Save();
+            }
 
             if (_score == _maxPossibleScore)
             {
