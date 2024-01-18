98d934e6732dea13ad42089be27d18e5b2eab6ea
Xwilarg <xwilarg@protonmail.com>
2024-01-18 17:08:33 +0100

    Remove unused usings

--- Assets/Scripts/Achievement/AchievementManager.cs
+++ Assets/Scripts/Achievement/AchievementManager.cs
@@ -1,9 +1,7 @@
 ﻿using NsfwMiniJam.Persistency;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace NsfwMiniJam.Achievement
 {
