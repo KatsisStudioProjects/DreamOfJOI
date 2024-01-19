be04d8bc6bb93d61b0935534a31e5b95d2a5f7f6
Xwilarg <xwilarg@protonmail.com>
2024-01-19 11:53:06 +0100

    Add one cloud on worldmap

---
+++ Assets/Scripts/LevelSelect/SetYLayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace NsfwMiniJam.LevelSelect
+{
+    public class SetYLayer : MonoBehaviour
+    {
+        private SpriteRenderer _sr;
+
+        [SerializeField]
+        private float _offset;
+
+        private void Awake()
+        {
+            _sr = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            _sr.sortingOrder = (int)(-(transform.position.y + _offset) * 100f + 10_000_000f);
+        }
+    }
+}
